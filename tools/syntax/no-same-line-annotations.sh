40cd921dbf9b38cb5a85b2699c09f447f326f0cc
twalters <timlwalters@yahoo.co.uk>
2026-01-11 04:03:30 +0000

    handle upto 3 modifiers

--- tools/syntax/no-same-line-annotations.sh
+++ tools/syntax/no-same-line-annotations.sh
@@ -19,15 +19,17 @@ f=$1  # file
 # need "{" to make sure it matches methods and not variables
 # only matching simple annotations, eg: @Nullable
 # won't work if annotation has params, eg: @Nullable(false)
-# handle static then a keyword
-sed "${SED_OPTIONS[@]}" 's/^([[:space:]]*)(static)[[:space:]](public|private|protected)[[:space:]](@[0-9A-Za-z_\$£]*)[[:space:]](.*\{)/\1\4 \2 \3 \5/' "$f"
-# handle a keyword static
-sed "${SED_OPTIONS[@]}" 's/^([[:space:]]*)(public|private|protected)[[:space:]](static)[[:space:]](@[0-9A-Za-z_\$£]*)[[:space:]](.*\{)/\1\4 \2 \3 \5/' "$f"
-# handle a single keyword - must be last one so it doesn't interfere with others
-sed "${SED_OPTIONS[@]}" 's/^([[:space:]]*)(public|private|protected|static)[[:space:]](@[0-9A-Za-z_\$£]*)[[:space:]](.*\{)/\1\3 \2 \4/' "$f"
+modifiers='(public|private|protected|static|abstract|final)'
+# handle three modifiers before annotation
+sed "${SED_OPTIONS[@]}" 's/^([[:space:]]*)'"$modifiers"'[[:space:]]+'"$modifiers"'[[:space:]]+'"$modifiers"'[[:space:]]+(@[0-9A-Za-z_\$£]*)[[:space:]]+(.*\{)/\1\5 \2 \3 \4 \6/' "$f"
+# handle two modifiers before annotation
+sed "${SED_OPTIONS[@]}" 's/^([[:space:]]*)'"$modifiers"'[[:space:]]+'"$modifiers"'[[:space:]]+(@[0-9A-Za-z_\$£]*)[[:space:]]+(.*\{)/\1\4 \2 \3 \5/' "$f"
+# handle one modifier - must be last one so it doesn't interfere with others
+sed "${SED_OPTIONS[@]}" 's/^([[:space:]]*)'"$modifiers"'[[:space:]]+(@[0-9A-Za-z_\$£]*)[[:space:]]+(.*\{)/\1\3 \2 \4/' "$f"
 
 # move the annotation to it's own line
-sed "${SED_OPTIONS[@]}" 's/^([[:space:]]*)(@[0-9A-Za-z_\$£]*)[[:space:]](.*\{)/\1\2\n\1\3/' "$f"
-
-# TODO might be a way to handle multiple modifiers in one command
-# TODO are there other modifiers?
+# TODO this matches method params that have been wrapped over to a new line
+# which also means, annotations in the first line will not be fixed as they don't have the "{"
+# how to exclude those?
+# perhaps could use a multiline matcher until the "{" is matched
+sed "${SED_OPTIONS[@]}" 's/^([[:space:]]*)(@[0-9A-Za-z_\$£]*)[[:space:]]+(.*\{)/\1\2\n\1\3/' "$f"
